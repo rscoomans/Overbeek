b8ae1383e57633af5553b081b7effbc8356092eb
rscoomans <reneecoomans@gmail.com>
2014-04-04 10:33:51 +0200

    titel

--- week_1/index.php
+++ week_1/index.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-        <title></title>
+        <title>Hoe istie</title>
     </head>
     <body>
         <?php
-        echo"Hallo daar<br />";
-        echo"Hoe gaat het ermee?"
+        echo"Hallo daar.<br />";
+        echo"Hoe gaat het ermee?";
         ?>
     </body>
 </html>
